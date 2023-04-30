eb7569c4e23211589e334a004830aadab4f532f2
Harmandeep Singh <singhharmandeep31@icloud.com>
2023-04-30 14:11:30 -0700

    removed old go-api

--- go-api/models/courseModel.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
-type Course struct {
-	Id primitive.ObjectID `json:"_id,omitempty"`
-	Name string `json:"name,omitempty" validate:"required"`
-	Description string `json:"description,omitempty" validate:"required"`
-}
