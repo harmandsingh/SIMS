a9a3bf9d9d14a0873772cfea222b3b2bc2c4fdfb
Maninderjit Singh <maninderjitsingh@sjsu.edu>
2023-05-13 02:12:20 -0700

    saved changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,12 +5,8 @@ import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./scenes/layout";
-<<<<<<< HEAD
-import Homepage from "@/scenes/pages/homepage";
-=======
 import { themeSettings } from "./theme";
 import Students from "./scenes/students";
->>>>>>> f1f4b4563c6ae85ca1b20c2e1bb03b0807f558ad
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
@@ -27,13 +23,8 @@ function App() {
                   element={<Navigate to="/dashboard" replace />}
                 />
                 <Route path="/login" element={<Login />} />
-<<<<<<< HEAD
-                <Route path="/profile" element={<div>Profile Page</div>} />
-                <Route path="/homepage" element={<Homepage/>} />
-=======
                 <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/students" element={<Students />} />
->>>>>>> f1f4b4563c6ae85ca1b20c2e1bb03b0807f558ad
               </Route>
             </Routes>
           </Box>
--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,24 +1,7 @@
 import { Box, useTheme } from "@mui/material";
-<<<<<<< HEAD
-import useSWR from "swr";
-
-type Props = {};
-
-export const ENDPOINT = "BASE_URL";
-
-const fetcher = (url: string) =>
-  fetch(`${ENDPOINT}/${url}`).then((res) => res.json());
-
-const Dashboard = (props: Props) => {
-  // const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)");
-  const { palette } = useTheme();
-
-  const { data } = useSWR<GetStudentsResponse>("students", fetcher);
-=======
 
 const Dashboard = () => {
   const theme = useTheme();
->>>>>>> f1f4b4563c6ae85ca1b20c2e1bb03b0807f558ad
 
   return (
     <Box
@@ -26,15 +9,8 @@ const Dashboard = () => {
       width="100%"
       display="grid"
       gap="1.5rem"
-<<<<<<< HEAD
-      sx={{ color: palette.primary.main[500] }}
-    >
-      {data?.data && JSON.stringify(data.data)}
-    </Box>
-=======
       sx={{ color: theme.palette.grey[100] }}
     ></Box>
->>>>>>> f1f4b4563c6ae85ca1b20c2e1bb03b0807f558ad
   );
 };
 
