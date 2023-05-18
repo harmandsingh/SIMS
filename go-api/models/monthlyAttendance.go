b7e7483e29cd42b94df2c3657c9fc3137cab984f
Maninderjit Singh <maninderjitsingh@sjsu.edu>
2023-05-18 16:50:08 -0700

    save

--- go-api/models/monthlyAttendance.go
+++ go-api/models/monthlyAttendance.go
@@ -1,6 +1,9 @@
 package models
 
-type monthlyAttendance struct {
-	Month    string `json:"date" bson:"date"`
-	totalAtt int    `json: "totalAtt" bson:"totalAtt"`
-}
+// import "go.mongodb.org/mongo-driver/bson/primitive"
+
+// type monthlyAttendance struct {
+// 	ID       primitive.ObjectID `json:"id" bson:"_id"`
+// 	Month    string             `json:"date" bson:"date"`
+// 	totalAtt int                `json: "totalAtt" bson:"totalAtt"`
+// }
