c51a6eceed5324e6ce26b755756425c3e0be6bd8
Harmandeep Singh <singhharmandeep31@icloud.com>
2023-05-18 18:13:42 -0700

    Added dashboard statbox

--- client/src/components/Statbox.tsx
+++ client/src/components/Statbox.tsx
@@ -1,5 +1,5 @@
 import { SvgIconComponent } from "@mui/icons-material";
-import { Box, Icon, Typography, useTheme } from "@mui/material";
+import { Box, SvgIcon, Typography, useTheme } from "@mui/material";
 import FlexBetween from "./FlexBetween";
 
 interface StatBoxProps {
@@ -21,16 +21,36 @@ const StatBox = ({ title, value, icon, description }: StatBoxProps) => {
         justifyContent: "space-between",
         p: "1.25rem 1rem",
         flex: "1 1 100%",
-        backgroundColor: theme.palette.background.default,
+        backgroundColor: theme.palette.primary.main,
         borderRadius: "0.55rem",
       }}
     >
       <FlexBetween>
-        <Typography>{title}</Typography>
-        <Icon>
-          <icon />
-        </Icon>
+        <Typography variant="h4" fontWeight={600} color="#21295c">
+          {title}
+        </Typography>
+        <SvgIcon
+          component={icon}
+          sx={{
+            fontSize: 50,
+            color: "#21295c",
+          }}
+        />
+      </FlexBetween>
+      <Typography
+        variant="h3"
+        fontWeight="bold"
+        color={theme.palette.secondary.dark}
+      >
+        {value}
+      </Typography>
+      <FlexBetween gap="2rem">
+        <Typography variant="h6" color={"#4d547d"}>
+          {description}
+        </Typography>
       </FlexBetween>
     </Box>
   );
 };
+
+export default StatBox;
--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,31 +1,63 @@
 import { getUsername, isAuthenticated } from "@/App";
+import { getAllStudents } from "@/api/students.service";
+import FlexBetween from "@/components/FlexBetween";
 import Header from "@/components/Header";
-import { Box, useTheme } from "@mui/material";
-import { useEffect } from "react";
+import StatBox from "@/components/Statbox";
+import { Student } from "@/types/student";
+import { Box, useMediaQuery, useTheme } from "@mui/material";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import PersonIcon from "@mui/icons-material/Person";
 
 const Dashboard = () => {
   const theme = useTheme();
   const navigate = useNavigate();
   const userName = getUsername();
-  const greeting = `Hello, ${userName}`;
+  const greeting = `Hello, ${userName}!`;
+  const [students, setStudents] = useState<Student[] | null>([]);
+  const [error, setError] = useState();
+  const isNonMobile = useMediaQuery("(min-width: 1000px)");
 
   useEffect(() => {
+    // Check if the user is authenticated
     if (!isAuthenticated()) {
       navigate("/login");
     }
-  });
+
+    // Get all students
+    getAllStudents()
+      .then((result) => setStudents(result))
+      .catch((error) => setError(error));
+  }, []);
 
   return (
-    <Box
-      height="100%"
-      width="100%"
-      display="grid"
-      gap="1.5rem"
-      m="2rem 1.25rem"
-      sx={{ color: theme.palette.grey[100] }}
-    >
-      {userName && <Header title={greeting} subtitle="" />}
+    <Box m="2rem 1.25rem">
+      {userName && (
+        <Header title={greeting} subtitle="Welcome to your Dashboard" />
+      )}
+      <Box
+        mt="25px"
+        display="flex"
+        justifyContent="space-between"
+        rowGap="20px"
+        columnGap="2%"
+        sx={{
+          "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
+        }}
+      >
+        <StatBox
+          title="Students"
+          value={students ? students.length : 0}
+          description="Total Students Enrolled"
+          icon={PersonIcon}
+        />
+        <StatBox
+          title="Teachers"
+          value={students ? students.length : 0}
+          description="Total Teachers Count"
+          icon={PersonIcon}
+        />
+      </Box>
     </Box>
   );
 };
