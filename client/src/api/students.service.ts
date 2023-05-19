bace05e9fc301e8099eedb0c14e344644a9dca0c
Harmandeep Singh <singhharmandeep31@icloud.com>
2023-05-19 00:11:27 -0700

    updated api for token expiration

--- client/src/api/students.service.ts
+++ client/src/api/students.service.ts
@@ -1,10 +1,15 @@
-import axios from "axios";
-import authHeader from "./auth-header";
-import { API_URL } from "./auth.service";
 import { GetStudentsResponse } from "@/types/student";
+import axios, { AxiosError } from "axios";
+import authHeader from "./auth.header";
+import { API_URL, logout } from "./auth.service";
 
 export const getAllStudents = async () => {
   return await axios
     .get<GetStudentsResponse>(API_URL + "students", { headers: authHeader() })
-    .then((response) => response.data.data);
+    .then((response) => response.data.data)
+    .catch((error: AxiosError) => {
+      if (error.code === AxiosError.ERR_BAD_REQUEST) {
+        logout();
+      }
+    });
 };
