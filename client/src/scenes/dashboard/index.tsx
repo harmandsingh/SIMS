27ab5ba93317747e5c4c1cc4083aa6262109bec6
Harmandeep Singh <singhharmandeep31@icloud.com>
2023-05-19 11:52:56 -0700

    dashboard update

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -2,6 +2,7 @@ import { getUsername, isAuthenticated } from "@/App";
 import { getAllClasses } from "@/api/classes.service";
 import { getAllStudents } from "@/api/students.service";
 import { getAllTeachers } from "@/api/teachers.service";
+import FlexBetween from "@/components/FlexBetween";
 import Header from "@/components/Header";
 import StatBox from "@/components/Statbox";
 import { Class } from "@/types/class";
@@ -10,10 +11,24 @@ import { Teacher } from "@/types/teacher";
 import ClassIcon from "@mui/icons-material/Class";
 import Face3Icon from "@mui/icons-material/Face3";
 import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
-import { Box, useMediaQuery, useTheme } from "@mui/material";
+import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from "recharts";
+import {
+  Bar,
+  BarChart,
+  Cell,
+  Legend,
+  Line,
+  LineChart,
+  Pie,
+  PieChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+import { ClassBarData } from "../studentClassRatio";
 
 const data = [
   { name: "Boy", value: 400 },
@@ -31,6 +46,38 @@ const Dashboard = () => {
   const [error, setError] = useState();
   const isNonMobile = useMediaQuery("(min-width: 1000px)");
 
+  const COLORS = [theme.palette.primary.main, theme.palette.secondary.main];
+
+  let classData: ClassBarData[] = [];
+
+  function getSingleClass(singleClass: Class) {
+    const classData: ClassBarData = {
+      name: singleClass.name,
+      studentCount: singleClass.students.length,
+    };
+    return classData;
+  }
+
+  if (classes) {
+    classes.map((singleClass) => {
+      if (singleClass) {
+        if (singleClass.students) {
+          const result: ClassBarData = {
+            name: singleClass.name,
+            studentCount: singleClass.students.length,
+          };
+          classData.push(result);
+        } else {
+          const result: ClassBarData = {
+            name: singleClass.name,
+            studentCount: 0,
+          };
+          classData.push(result);
+        }
+      }
+    });
+  }
+
   useEffect(() => {
     // Check if the user is authenticated
     if (!isAuthenticated()) {
@@ -39,20 +86,26 @@ const Dashboard = () => {
 
     // Get all students
     getAllStudents()
-      .then((result) => setStudents(result!!))
+      .then((result) => {
+        result && setStudents(result);
+      })
       .catch((error) => setError(error));
 
     getAllTeachers()
-      .then((result) => setTeachers(result!!))
+      .then((result) => {
+        result && setTeachers(result);
+      })
       .catch((error) => setError(error));
 
     getAllClasses()
-      .then((result) => setClasses(result))
+      .then((result) => {
+        result && setClasses(result);
+      })
       .catch((error) => setError(error));
   }, []);
 
   return (
-    <Box m="1.5rem 1.25rem">
+    <Box height="85%" width="95%" m="1.5rem 1.25rem">
       {userName && (
         <Header title={greeting} subtitle="Welcome to your Dashboard" />
       )}
@@ -85,6 +138,62 @@ const Dashboard = () => {
           icon={ClassIcon}
         />
       </Box>
+      <FlexBetween height="75%" width="90%" mt="2rem" columnGap="2rem">
+        <>
+          <Typography
+            color={theme.palette.secondary.main}
+            variant="h4"
+            m="6rem 0rem -2.5rem 2rem"
+          >
+            Gender Ratio
+          </Typography>
+          <ResponsiveContainer width="50%" height="85%">
+            <PieChart width={600} height={600}>
+              <Pie
+                dataKey="value"
+                data={data}
+                cx="50%"
+                cy="50%"
+                innerRadius={115}
+                outerRadius={230}
+                fill={theme.palette.primary.main}
+                label
+              >
+                {data.map((_entry, index) => (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={COLORS[index % COLORS.length]}
+                  />
+                ))}
+              </Pie>
+              <Legend />
+              <Tooltip />
+            </PieChart>
+          </ResponsiveContainer>
+        </>
+        <>
+          <ResponsiveContainer width="80%" height="90%">
+            <BarChart
+              width={500}
+              height={300}
+              data={classData}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="studentCount" fill={theme.palette.primary.main} />
+              {/* <Bar dataKey="uv" fill="#82ca9d" /> */}
+            </BarChart>
+          </ResponsiveContainer>
+        </>
+      </FlexBetween>
     </Box>
   );
 };
