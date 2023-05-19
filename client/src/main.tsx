f665544867843358042046c39b8b10a700952502
Harmandeep Singh <singhharmandeep31@icloud.com>
2023-05-18 17:16:50 -0700

    react main app update

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,12 +2,9 @@ import App from "@/App.tsx";
 import "@/index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RecoilRoot } from "recoil";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RecoilRoot>
-      <App />
-    </RecoilRoot>
+    <App />
   </React.StrictMode>
 );
