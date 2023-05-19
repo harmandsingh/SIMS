cad45ce821ed1fd5d34f8f4a3d069edea801d6fe
Maninderjit Singh <maninderjitsingh@sjsu.edu>
2023-05-18 17:33:28 -0700

    saving local changes

---
+++ go-api/routes/monthlyAttendance.go
@@ -0,0 +1,14 @@
+package routes
+
+// import (
+// 	"go-api/controllers"
+
+// 	"github.com/gofiber/fiber/v2"
+// )
+
+// func monthlyAttendance(app *fiber.App) {
+// 	gradeGroup := app.Group("api/v1/homepage/")
+
+// 	gradeGroup.Get("/monthlyAttendance", controllers.GetAttendance) //overall attendance
+// 	gradeGroup.Get("/:id", controllers.GetAttendanceByMonth)        //per month attendance
+// }
